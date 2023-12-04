8d56ce5685e867664438f2cbfc171da70f7461c9
yura-pepsicola <y.nosuch@gmail.com>
2023-12-04 15:06:30 +0200

    a

--- ynosach-3/src/mx_output_columns_x.c
+++ ynosach-3/src/mx_output_columns_x.c
@@ -8,7 +8,7 @@ void mx_output_columns_x(t_li **names, int rows, int cols, int maxlen) {
         for (; names[j] && j < cols; j++) {
             mx_printstr_g(names[j]);
             if (names[j + 1] && (j != cols - 1))
-                mx_print_spaces_g(mx_strlen(names[j]->name), maxlen);
+                mx_output_spaces_g(mx_strlen(names[j]->name), maxlen);
         }
         if (i != rows - 1)
             mx_printchar('\n');
